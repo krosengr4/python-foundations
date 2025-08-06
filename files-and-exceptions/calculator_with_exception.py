a5b9dc1a73f1f5d1f88699462c108d14c4f89bf2
Kevin Rosengren <rosenkev4@gmail.com>
2025-08-06 02:15:20 -0500

    Added more notes on exceptions

---
+++ files-and-exceptions/calculator_with_exception.py
@@ -0,0 +1,13 @@
+def remainder_division(a, b):
+    if b == 0:
+        raise ZeroDivisionError
+    
+    # Remember // means that it will divide to a whole number
+    result = a // b
+    remainder = a % b
+    print(a, 'divided by', b, 'is', result, 'with a remainder of', remainder)
+
+num1 = int(input('\nEnter a number:\n'))
+num2 = int(input('Enter another number:\n'))
+
+remainder_division(num1, num2)
