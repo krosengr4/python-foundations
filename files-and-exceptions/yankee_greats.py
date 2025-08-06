1e291bcbfea3f51b3797eed9c949b072368de006
Kevin Rosengren <rosenkev4@gmail.com>
2025-08-06 02:54:18 -0500

    Refined excersise on reading and writing to files

--- files-and-exceptions/yankee_greats.py
+++ files-and-exceptions/yankee_greats.py
@@ -1,26 +1,34 @@
+file_path = 'files-and-exceptions/yankee_greats.txt'
+
 def find_yankee():
     user_search = input('\nEnter the number of the Yankee to search:\n')
 
-    found = False
-    with open('files-and-exceptions/yankee_greats.txt') as file:
-        for line in file:
-            if user_search in line:
-                print(line)
-                found = True
-                break
-    
-    if not found:
-        print("There is no Yankee with that number...")
+    try:
+        found = False
+        with open(file_path) as file:
+            for line in file:
+                if user_search in line:
+                    print(line)
+                    found = True
+                    break
+        
+        if not found:
+            print("There is no Yankee with that number...")
+    except FileNotFoundError as e:
+        print("The file was not found...")
 
 def add_yankee():
     number = input("\nWhat is the number of the Yankee:\n")
     fname = input("What is the first name of the Yankee:\n").capitalize()
     lname = input('What is the last name of the yankee:\n').capitalize()
 
-    with open('files-and-exceptions/yankee_greats.txt', 'a') as file:
-        file.write('\n' + number + ' - ' + fname + ' ' + lname)
+    try: 
+        with open(file_path, 'a') as file:
+            file.write('\n' + number + ' - ' + fname + ' ' + lname)
 
-    print('The Yankee was added!')
+        print('The Yankee was added!')
+    except FileNotFoundError as e:
+        print("The file was not found...")
     
 def main():
     user_choice = input("\nWould you like to search(S) or add(A) a Yankee:\n").upper()
