faf16313dc0c2e8048ed6462a6f0e98d927a928f
Kevin Rosengren <rosenkev4@gmail.com>
2025-08-13 17:43:21 -0500

    Refined the dice_game so that it doesn't ask for the players names every time the game is played

--- functions-notes/dice_game.py
+++ functions-notes/dice_game.py
@@ -1,24 +1,45 @@
 import random
 
-def roll_dice():
-    return random.randint(1, 6) + random.randint(1, 6);
+# def roll_dice():
+#     return random.randint(1, 6) + random.randint(1, 6);
+
+def roll1():
+    return random.randint(1,6)
+
+def roll2():
+    return random.randint(1,6)
 
 
 ifContinue = True
 
+player1 = input('\nEnter the name of player one:\n')
+player2 = input('Enter the name of player two:\n')
+
 while(ifContinue):
-    player1 = input('\nEnter the name of player one:\n')
-    player2 = input('Enter the name of player two:\n')
 
-    roll1 = roll_dice();
-    roll2 = roll_dice();
+    player1_roll1 = roll1();
+    print(player1 + 's first dice is a ', player1_roll1)
+    input('Hit enter to continue')
 
-    print('\n' + player1, 'rolled:', roll1)
-    print(player2, 'rolled:', roll2)
+    player1_roll2 = roll2();
+    print(player1 + 's second dice is a', str(player1_roll2))
+    input('Hit enter to continue')
 
-    if roll1 > roll2: 
+    player2_roll1 = roll1();
+    print(player2 + 's first dice is a', str(player2_roll1))
+    input('Hit enter to continue')
+
+    player2_roll2 = roll2();
+    print(player2 + 's second dice is a', str(player2_roll2))
+    input('Hit enter to continue')
+
+    player1_total = player1_roll1 + player1_roll2
+    player2_total = player2_roll1 + player2_roll2
+
+
+    if player1_total > player2_total: 
         print(player1, 'wins!\n');
-    elif roll1 < roll2:
+    elif player1_total < player2_total:
         print(player2, 'wins!\n');
     else:
         print('It was a tie...\n');
